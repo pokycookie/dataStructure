c46bd452751d982fd2e1257f5a0ab8937cb82f23
pokycookie <0cookieboy0@gmail.com>
2022-11-23 01:21:28 +0900

    Update bst.cpp

--- bst.cpp
+++ bst.cpp
@@ -38,7 +38,6 @@ private:
 		printf("%c ", start->getKey());
 		if (start->getLeft() != NULL) traverse(start->getLeft());
 		if (start->getRight() != NULL) traverse(start->getRight());
-		
 	}
 public:
 	// 생성자
@@ -78,6 +77,29 @@ public:
 			}
 		}
 	}
+	// key값 찾기
+	Node* search(char key) {
+		Node* tempNode = root;
+		int level = 1;
+		while (1) {
+			if (key < tempNode->getKey()) {
+				if (tempNode->getLeft() == NULL) break;
+				tempNode = tempNode->getLeft();
+			}
+			else if (key > tempNode->getKey()) {
+				if (tempNode->getRight() == NULL) break;
+				tempNode = tempNode->getRight();
+			}
+			else {
+				printf("\n[%d] %c를 찾았습니다.\n",level, key);
+				return tempNode;
+			}
+			level++;
+		}
+		printf("\n찾는 키가 존재하지 않습니다.\n");
+		return NULL;
+	}
+	
 	// 트리구조 이미지화
 	void printBST() {
 		Node* tempNode = root;
@@ -89,9 +111,14 @@ int main() {
 	BST myBST = BST('G');
 	myBST.insert('Z');
 	myBST.insert('D');
+	myBST.insert('C');
+	myBST.insert('F');
 	myBST.insert('A');
 	myBST.insert('S');
+	myBST.insert('T');
 	myBST.insert('K');
 	myBST.printBST();
+	myBST.search('A');
+	myBST.search('M');
 	return 0;
 }
